a3cabb8973ed41bac557ae2f6e9cab5f410aec16
davejang <davejang98@naver.com>
2023-10-09 22:51:25 +0900

    feat: Member Domain
    Member Domain with JPA Entity

---
+++ core/src/main/java/davejang/core/domain/Member.java
@@ -0,0 +1,29 @@
+package davejang.core.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    private Long Id;
+    private String name;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
