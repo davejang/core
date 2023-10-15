70a95d662317a101663da409ddc5007e8c27155d
KyungSeok Jang <davejang98@naver.com>
2023-10-15 15:35:15 +0900

    refactor: Move Root directory

---
+++ src/main/java/davejang/core/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package davejang.core.repository;
+
+import davejang.core.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
